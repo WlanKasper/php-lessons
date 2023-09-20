59bef616d0c65c9ef2e56b8e13cd5181b4ef00b3
WlanKasper <wlankasper@gmail.com>
2023-09-20 21:41:12 +0200

    initial commit

---
+++ php/src/index.php
@@ -0,0 +1,14 @@
+<?php
+// index.php
+// Il nome del servizio definito in docker-compose.yml.
+$host = 'db';
+$user = 'MYSQL_USER';
+$pass = 'MYSQL_PASSWORD';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+?>
