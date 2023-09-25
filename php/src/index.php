fa37f9b80275b0db08a51bc78017e45be44f26a2
WlanKasper <wlankasper@gmail.com>
2023-09-25 17:30:06 +0200

    wip

--- php/src/index.php
+++ php/src/index.php
@@ -1,13 +1,24 @@
-<?php
-// index.php
-$host = 'db';
-$user = 'MYSQL_USER';
-$pass = 'MYSQL_PASSWORD';
-$conn = new mysqli($host, $user, $pass);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base PHP</title>
+</head>
+<body>
+    <header>
+        <!-- Free -->
+    </header>
+    <div class="container">
+        <div class="menu">
+            
+        </div>
+        <div class="display">
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
-?>
+        </div>
+    </div>
+    <footer>
+        <!-- Free -->
+    </footer>
+</body>
+</html>
